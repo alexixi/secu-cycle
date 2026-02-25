b846a6976a7f37230407b8044e9842dde8650012
Alexis Gaudray Bouju <alexis.gaudray_bouju@enseirb-matmeca.fr>
2026-02-25 19:04:48 +0100

    [frontend] Création de la page d'accueil

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,50 @@
 import Header from "../components/layout/Header"
+import './HomePage.css';
+import IconButton from "../components/ui/IconButton";
+import { IoIosArrowDropdown } from "react-icons/io";
 
 export default function HomePage() {
-  return (
-    <>
-    <Header page="home" />
-    <div>
-      <h1>Itinéraire à vélo</h1>
-      <p>Bienvenue sur notre application de planification d'itinéraires à vélo.</p>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <Header page="home" />
+            <div id="container-top-homepage">
+                <picture>
+                    <source className="logo-homepage" srcSet="src/assets/logo_clair.png" media="(prefers-color-scheme: dark)" alt="Logo Sécu'Cycle" />
+                    <img className="logo-homepage" src="src/assets/logo.png" alt="Logo Sécu'Cycle" />
+                </picture>
+                <div>
+                    <h1 id="title-homepage">Sécu'Cycle</h1>
+                    <p>Découvrez le projet</p>
+                </div>
+                <IconButton
+                    onClick={
+                        () => document.getElementById("home-faq-section").scrollIntoView({ behavior: "smooth", block: "start" })
+                    }
+                    className="scroll-button"
+                >
+                    <IoIosArrowDropdown size={40} className="arrow-down" />
+                </IconButton>
+            </div>
+            <div id="home-faq-section">
+                <section className="home-section">
+                    <h2>Qu'est-ce que Sécu'Cycle ?</h2>
+                    <p>
+                        <em>Sécu'Cycle</em> est un projet développé par 6 étudiants de <em>l'ENSEIRB-MATMECA</em> dans le cadre d'un PFA. L'objectif de ce projet est de créer une application web et mobile qui aide les cyclistes à trouver des itinéraires sécurisés en fonction de leurs préférences et de leur profil. Nous nous sommes focalisés sur la zone de Bordeaux et de notre campus universitaire pour affiner les résultats avec nos connaissances locals du terrain.
+                    </p>
+                </section>
+                <section className="home-section">
+                    <h2>Pourquoi Sécu'Cycle ?</h2>
+                    <p>
+                        Ce projet s'inscrit dans une démarche de promotion de la <em>mobilité douce</em> et de la sécurité des cyclistes. En fournissant des itinéraires adaptés, Sécu'Cycle vise à encourager davantage de personnes à adopter le vélo comme moyen de transport quotidien, tout en réduisant les risques d'accidents.
+                    </p>
+                </section>
+                <section className="home-section">
+                    <h2>Source des données</h2>
+                    <p>
+                        <em>Sécu'Cycle</em> combine différentes sources de données, principalement les données d'<em>OpenStreetMap</em> pour la carte des routes et pistes cyclables. Nous utilisons d'autes sources comme les données d'accident de la route pour évaluer la sécurité des itinéraires.
+                    </p>
+                </section>
+            </div>
+        </>
+    )
 }
