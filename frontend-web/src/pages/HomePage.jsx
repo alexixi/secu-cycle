c3638e0ec8de1fedc927fd9f6ac4500cb853403e
Alexis Gaudray Bouju <alexis.gaudray_bouju@enseirb-matmeca.fr>
2026-05-02 17:00:01 +0200

    [frontend] Amélioration page d'acceuil avec ajout de la section de promotion de l'application

--- frontend-web/src/pages/HomePage.jsx
+++ frontend-web/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from "../components/layout/Header"
 import './HomePage.css';
 import IconButton from "../components/ui/IconButton";
 import Logo from "../assets/logo.svg?react";
+import apercuApplication from "../assets/screenshots/apercu_itineraire-left.png";
 import { IoIosArrowDropdown } from "react-icons/io";
 
 export default function HomePage() {
@@ -29,21 +30,39 @@ export default function HomePage() {
                 <section className="home-section">
                     <h2>Qu'est-ce que Sécu'Cycle ?</h2>
                     <p>
-                        <em>Sécu'Cycle</em> est un projet développé par 6 étudiants de <em>l'ENSEIRB-MATMECA</em> dans le cadre d'un PFA. L'objectif de ce projet est de créer une application web et mobile qui aide les cyclistes à trouver des itinéraires sécurisés en fonction de leurs préférences et de leur profil. Nous nous sommes focalisés sur la zone de Bordeaux et de notre campus universitaire pour affiner les résultats avec nos connaissances locals du terrain.
+                        <em>Sécu'Cycle</em> est un projet développé par 6 étudiants de <em>l'ENSEIRB-MATMECA</em> dans le cadre d'un PFA. L'objectif de ce projet est de créer un site web et une application mobile qui aide les cyclistes à trouver des itinéraires sécurisés en fonction de leurs préférences et de leur profil et de leur équipement. Nous nous sommes focalisés sur la zone de Bordeaux et de notre campus universitaire pour affiner les résultats avec nos connaissances locals du terrain.
                     </p>
                 </section>
                 <section className="home-section">
                     <h2>Pourquoi Sécu'Cycle ?</h2>
                     <p>
-                        Ce projet s'inscrit dans une démarche de promotion de la <em>mobilité douce</em> et de la sécurité des cyclistes. En fournissant des itinéraires adaptés, Sécu'Cycle vise à encourager davantage de personnes à adopter le vélo comme moyen de transport quotidien, tout en réduisant les risques d'accidents.
+                        Ce projet s'inscrit dans une démarche de promotion des <em>mobilités douces</em> et de la sécurité des cyclistes. En fournissant des itinéraires adaptés, Sécu'Cycle vise à encourager davantage de personnes à adopter le vélo comme moyen de transport quotidien, tout en réduisant les risques d'accidents.
                     </p>
                 </section>
                 <section className="home-section">
-                    <h2>Source des données</h2>
+                    <h2>Sources des données</h2>
                     <p>
-                        <em>Sécu'Cycle</em> combine différentes sources de données, principalement les données d'<em>OpenStreetMap</em> pour la carte des routes et pistes cyclables. Nous utilisons d'autes sources comme les données d'accident de la route pour évaluer la sécurité des itinéraires.
+                        <em>Sécu'Cycle</em> combine différentes sources de données, principalement les données d'<a href="https://www.openstreetmap.fr/" target="_blank" rel="noopener noreferrer"><em>OpenStreetMap</em></a> pour la carte des routes et pistes cyclables. Nous utilisons d'autes sources comme la <a href="https://adresse.data.gouv.fr/" target="_blank" rel="noopener noreferrer">BAN</a> (Base Adresse Nationale) pour la complétion des adresses et les données de trafic du projet <a href="https://avatar.cerema.fr/" target="_blank" rel="noopener noreferrer"><em>Avatar</em></a> du Cerema. Pour l'affichage de la carte, nous utilisons <a href="https://www.maptiler.com/" target="_blank" rel="noopener noreferrer"><em>MapTiler</em></a> qui propose des tuiles cartographiques basées sur les données d'OpenStreetMap.
                     </p>
                 </section>
+            </div>
+            <div id="app-section">
+                <img className="app-visual" src={apercuApplication} alt="Aperçu de l'application mobile" />
+                <aside>
+                    <h2>Découvrez notre application mobile</h2>
+                    <p>
+                        Téléchargez l'application mobile pour une expérience utilisateur optimisée pour la navigation en temps réel.
+                    </p>
+                    <div className="store-badges">
+                        <a href="#app-section">
+                            <img src="/store/appstore.svg" alt="Télécharger dans l'App Store" className="store-badge" />
+                        </a>
+
+                        <a href="#app-section">
+                            <img src="/store/googleplay.svg" alt="Disponible sur Google Play" className="store-badge" />
+                        </a>
+                    </div>
+                </aside>
             </div>
         </>
     )
