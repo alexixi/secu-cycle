f7b099dc1ade2db55ee4d56615696d97c942dbc0
Alexis Gaudray Bouju <alexis.gaudray_bouju@enseirb-matmeca.fr>
2026-07-16 16:42:41 +0200

    [frontend] forward des propriétés sur les boutons (pour metttre les aria-labels)

--- frontend-web/src/components/ui/IconButton.jsx
+++ frontend-web/src/components/ui/IconButton.jsx
@@ -1,8 +1,8 @@
 import './IconButton.css';
 
-export default function IconButton({ type, onClick, className, children }) {
+export default function IconButton({ type, onClick, className, children, ...rest }) {
     return (
-        <button type={type} className={`icon-button ${className}`} onClick={onClick}>
+        <button type={type} className={`icon-button ${className}`} onClick={onClick} {...rest}>
             {children}
         </button>
     );
